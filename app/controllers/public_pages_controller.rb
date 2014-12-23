ae071f3be872715ff91e0dfdd34ac0f2252c1303
Angelo Wong <angelowo@umich.edu>
2014-12-22 16:11:11 -0800

    init repo; init views for homepage signin signup contact

---
+++ app/controllers/public_pages_controller.rb
@@ -0,0 +1,10 @@
+class PublicPagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def signup
+  end
+end
---
+++ test/controllers/public_pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PublicPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+  test "should get signup" do
+    get :signup
+    assert_response :success
+  end
+
+end
