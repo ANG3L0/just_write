06d01d3bd1a5b87f83ef6e633018ccd9cc9a1df9
Ugghhh <angelowo@umich.edu>
2014-12-25 12:05:26 -0800

    add tests for article model

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@user = users(:angelo)
+		@article = @user.articles.build(content: "He sat on the wall, had a great fall", 
+																		title: "Humpty Dumpty")
+	end
+
+	test "should be able to have valid article" do
+		assert @article.valid?
+	end
+
+	test "should have user_id" do
+		@article.user_id = nil
+		assert_not @article.valid?
+	end
+	
+	test "should not be able to have empty content" do
+		@article.content = "   "
+		assert_not @article.valid?	
+	end
+
+	test "should not be able to have empty title" do
+		@article.title = "   "
+		assert_not @article.valid?	
+	end
+
+	test "title cannot be more than 140 characters" do
+		@article.title = "a" * 141
+		assert_not @article.valid?
+	end
 end
