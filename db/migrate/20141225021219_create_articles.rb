cfb24c92803319683f0f8e7956a7e1d5c72dd913
Angelo Wong <angelowong@Angelos-MacBook-Pro.local>
2014-12-24 19:52:04 -0800

    put away foreign_key restriction

--- db/migrate/20141225021219_create_articles.rb
+++ db/migrate/20141225021219_create_articles.rb
@@ -6,7 +6,7 @@ class CreateArticles < ActiveRecord::Migration
 
       t.timestamps null: false
     end
-    add_foreign_key :articles, :users
+    #add_foreign_key :articles, :users
     add_index :articles, [:user_id, :created_at]
   end
 end
