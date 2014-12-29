e618b946c9464c904787b6878f913f9f6c4c5005
Ugghhh <angelowo@umich.edu>
2014-12-29 11:06:48 -0800

    add tests for ajax voting

--- test/integration/article_voting_test.rb
+++ test/integration/article_voting_test.rb
@@ -13,15 +13,19 @@ class ArticleVotingTest < ActionDispatch::IntegrationTest
 		get user_url(@other)
 		assert_no_difference '@other_article.rating' do
 			patch upvote_article_path(@other_article.id), nil, { HTTPS: "on", HTTP_REFERER: user_url(@other) }
+			#xml http request version for js
+			xhr :patch, upvote_article_path(@other_article.id), nil, { HTTPS: "on", HTTP_REFERER: user_url(@other) }
 			@other_article.reload
 		end
 		assert_no_difference '@other_article.rating' do
 			patch downvote_article_path(@other_article.id), nil, { HTTPS: "on", HTTP_REFERER: user_url(@other) }
+			#xml http request version for js
+			xhr :patch, downvote_article_path(@other_article.id), nil, { HTTPS: "on", HTTP_REFERER: user_url(@other) }
 			@other_article.reload
 		end
 	end
 	
-	test "should be able to vote for other users, up or down" do
+	test "should be able to vote for other users, up or down (html)" do
 		log_in_as(@user)
 		get user_url(@other)
 		assert_difference '@other_article.rating', 1 do
@@ -33,8 +37,20 @@ class ArticleVotingTest < ActionDispatch::IntegrationTest
 			@other_article.reload
 		end
 	end
+	test "should be able to vote for other users, up or down (js)" do
+		log_in_as(@user)
+		get user_url(@other)
+		assert_difference '@other_article.rating', 1 do
+			xhr :patch, upvote_article_path(@other_article.id), nil, { HTTPS: "on", HTTP_REFERER: user_url(@other) }
+			@other_article.reload
+		end
+		assert_difference '@other_article.rating', -1 do
+			xhr :patch, downvote_article_path(@other_article.id), nil, { HTTPS: "on", HTTP_REFERER: user_url(@other) }
+			@other_article.reload
+		end
+	end
 
-	test "should be able to destroy other people's posts if voted below 0" do
+	test "should be able to destroy other people's posts if voted below 0 (html)" do
 		log_in_as(@user)
 		assert_difference 'Article.count', -1 do
 			patch downvote_article_path(@other_article.id), nil, { HTTPS: "on", HTTP_REFERER: user_url(@other) }
@@ -43,16 +59,28 @@ class ArticleVotingTest < ActionDispatch::IntegrationTest
 			patch downvote_article_path(@other_article.id), nil, { HTTPS: "on", HTTP_REFERER: user_url(@other) }
 		end
 	end
+	test "should be able to destroy other people's posts if voted below 0 (js)" do
+		log_in_as(@user)
+		assert_difference 'Article.count', -1 do
+			xhr :patch, downvote_article_path(@other_article.id), nil, { HTTPS: "on", HTTP_REFERER: user_url(@other) }
+			xhr :patch, downvote_article_path(@other_article.id), nil, { HTTPS: "on", HTTP_REFERER: user_url(@other) }
+			xhr :patch, downvote_article_path(@other_article.id), nil, { HTTPS: "on", HTTP_REFERER: user_url(@other) }
+			xhr :patch, downvote_article_path(@other_article.id), nil, { HTTPS: "on", HTTP_REFERER: user_url(@other) }
+		end
+		assert_not flash.empty?
+	end
 
 	test "should not be able to vote for myself" do
 		log_in_as(@user)
 		get user_url(@user)
 		assert_no_difference '@user_article.rating' do
 			patch upvote_article_path(@user_article.id), nil, { HTTPS: "on", HTTP_REFERER: user_url(@user) }
+			xhr :patch, upvote_article_path(@user_article.id), nil, { HTTPS: "on", HTTP_REFERER: user_url(@user) }
 			@user_article.reload
 		end
 		assert_no_difference '@user_article.rating' do
 			patch downvote_article_path(@user_article.id), nil, { HTTPS: "on", HTTP_REFERER: user_url(@user) }
+			xhr :patch, downvote_article_path(@user_article.id), nil, { HTTPS: "on", HTTP_REFERER: user_url(@user) }
 			@user_article.reload
 		end
 	end
