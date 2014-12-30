c10d149606e3b406bd7f95b23cb620d4832ff7e5
Ugghhh <angelowo@umich.edu>
2014-12-29 18:57:11 -0800

    init pagination for home

--- app/controllers/public_pages_controller.rb
+++ app/controllers/public_pages_controller.rb
@@ -1,7 +1,7 @@
 class PublicPagesController < ApplicationController
 
   def home
-		@articles = Article.all.published_and_in_score_order
+		@articles = Article.paginate(page: params[:page], per_page: 15).published_and_in_score_order
   end
 
   def contact
